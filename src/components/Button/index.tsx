515eabf85ce8ca757862fbef3e8fa6badfc5aa18
taemin <tmmoond8@gmail.com>
2020-05-21 01:28:23 +0900

    feat: Button 컴포넌트 작성 - emotion 추가

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,5 +1,9 @@
-import React, { ReactNode, MouseEventHandler } from 'react';
+/** @jsx jsx */
+import { jsx } from '@emotion/core';
+import styled from '@emotion/styled';
+import { ReactNode, MouseEventHandler } from 'react';
 import classnames from 'classnames';
+import { colors } from '../../styles/themes';
 
 interface ButtonProps {
   children: ReactNode;
@@ -7,9 +11,40 @@ interface ButtonProps {
   onClick: MouseEventHandler;
 }
 
-export default function Button (props: ButtonProps) {
+export default function(props: ButtonProps) {
   const { children, className='', onClick } = props;
   return (
-    <button onClick={onClick} className={classnames('Button', className)} type="button">{children}</button>
+    <Button onClick={onClick} className={classnames('Button', className)} type="button">{children}</Button>
   )
 }
+
+const Button = styled.button`
+  user-select: none;
+  transition: background 120ms ease-in 0s;
+  cursor: pointer;
+  display: inline-flex;
+  align-items: center;
+  justify-content: center;
+  flex-shrink: 0;
+  white-space: nowrap;
+  height: 28px;
+  border-width: 0;
+  border-radius: 3px;
+  background: ${colors.primaryLight};
+  box-shadow: rgba(15, 15, 15, 0.1) 0px 0px 0px 1px inset, rgba(15, 15, 15, 0.1) 0px 1px 2px;
+  color: white;
+  fill: white;
+  line-height: 1.2;
+  padding-left: 12px;
+  padding-right: 12px;
+  font-size: 14px;
+  font-weight: 500;
+  outline: none;
+
+  &:hover {
+    background: ${colors.primary};
+  }
+  &:active {
+    background: ${colors.primaryDeep};
+  }
+`;
---
+++ src/styles/themes/colors.ts
@@ -0,0 +1,9 @@
+const colors = {
+  primaryLight: '#2eabdc',
+  primary: '#069bcd',
+  primaryDeep: '#008dbe'
+}
+
+export default {
+  ...colors,
+}
