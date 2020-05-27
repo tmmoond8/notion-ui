23fe7c496c2ae7a68b86cf0cb4d672350915b668
taemin <tmmoond8@gmail.com>
2020-05-27 22:14:28 +0900

    feat: 버튼 베이스 컴포넌트 리팩토링

--- src/components/Button/ButtonBase.tsx
+++ src/components/Button/ButtonBase.tsx
@@ -1,9 +1,8 @@
 /** @jsx jsx */
-import { jsx, css } from '@emotion/core';
-import styled from '@emotion/styled';
-import { useState, useEffect, ReactNode, MouseEventHandler } from 'react';
+import { jsx, SerializedStyles } from '@emotion/core';
+import { ReactNode, MouseEventHandler, useMemo } from 'react';
 import classnames from 'classnames';
-import { colors } from 'src/styles/themes';
+import * as styles from './styles';
 
 export enum ButtonType {
   Default = 'Default',
@@ -26,128 +25,30 @@ interface ButtonProps {
   disabled?: boolean;
 }
 
-const useStyle = (buttonType: ButtonType, buttonSize: ButtonSize) => {
-  const [buttonTypeStyle, setButtonTypeStyle] = useState({
-    backgroundColor: colors.transparent,
-    backgroundColorHover: colors.greyR50,
-    backgroundColorActive: colors.greyR100,
-    boxShadow: 'none',
-    color: colors.grey800,
-  });
+const useSizeStyle = (buttonSize: ButtonSize): SerializedStyles => {
+  const sizeStyle = useMemo(() => styles.size[buttonSize], [buttonSize]);
+  return sizeStyle;
+}
 
-  const [buttonSizeStyle, setButtonSizeStyle] = useState({
-    height: '28px',
-    fontSize: '14px',
-    padding: '0 8px',
-  });
-
-  useEffect(() => {
-    if (buttonType === ButtonType.Primary) {
-      setButtonTypeStyle({
-        ...buttonTypeStyle,
-        backgroundColor: colors.primaryLight,
-        backgroundColorHover: colors.primary,
-        backgroundColorActive: colors.primaryDeep,
-        boxShadow: 'rgba(15, 15, 15, 0.1) 0px 0px 0px 1px inset, rgba(15, 15, 15, 0.1) 0px 1px 2px;',
-        color: colors.white,
-      });
-    }
-    if (buttonSize === ButtonSize.Small) {
-      setButtonSizeStyle({
-        ...buttonSizeStyle,
-        height: '24px',
-      });
-    } else if (buttonSize === ButtonSize.Big) {
-      setButtonSizeStyle({
-        ...buttonSizeStyle,
-        height: '32px',
-      });
-    } else if (buttonSize === ButtonSize.Tiny) {
-      setButtonSizeStyle({
-        ...buttonSizeStyle,
-        height: '20px',
-        fontSize: '12px',
-        padding: '0 4px',
-      });
-    }
-  }, [buttonType, buttonSize])
-
-  return {
-    ...buttonTypeStyle,
-    ...buttonSizeStyle,
-  };
+const useTypeStyle = (buttonType: ButtonType): SerializedStyles => {
+  const typeStyle = useMemo(() => styles.type[buttonType], [buttonType]);
+  return typeStyle;
 }
 
 export default function(props: ButtonProps) {
   const { children, className = '', onClick, disabled=false, buttonType=ButtonType.Default, buttonSize=ButtonSize.Medium } = props;
-  let buttonStyle = useStyle(buttonType, buttonSize);
+  const sizeStyle = useSizeStyle(buttonSize);
+  const typeStyle = useTypeStyle(buttonType);
 
   return (
-    <Button 
+    <button 
       onClick={onClick} 
       className={classnames('Button', className)} 
       type="button"
-      {...buttonStyle}
+      css={[styles.baseStyle, sizeStyle, typeStyle]}
       >
         {children}
-    </Button>
+    </button>
   );
 }
 
-interface ButtonStyles {
-  backgroundColor: string;
-  backgroundColorHover: string;
-  backgroundColorActive: string;
-  boxShadow: string;
-  color: string;
-  height: string;
-  fontSize: string;
-  padding: string;
-}
-
-const Button = styled.button<ButtonStyles, HTMLButtonElement>`
-  display: inline-block;
-  align-items: center;
-  flex-shrink: 0;
-  justify-content: center;
-  position: relative;
-  background-color: ${p => p.backgroundColor};
-  border: 0;
-  margin: 0;
-  height: ${p => p.height};
-  line-height: ${p => p.height};
-  cursor: pointer;
-  user-select: none;
-  vertical-align: middle;
-  appearance: none;
-  text-decoration: none;
-  transition: background 120ms ease-in 0s;
-
-  white-space: nowrap;
-  border-width: 0;
-  border-radius: 3px;
-  box-shadow: ${p => p.boxShadow};
-  color: ${p => p.color};
-  fill: ${p => p.color};
-  padding: ${p =>  p.padding};
-  font-size: ${p => p.fontSize};
-  font-weight: 500;
-  outline: none;
-
-  &:hover {
-    background-color: ${p => p.backgroundColorHover};
-  }
-  &:active {
-    background-color: ${p => p.backgroundColorActive};
-  }
-  &::-moz-focus-inner {
-    border-style: none;
-  }
-  /* disabled {
-      pointerEvents: none;
-      cursor: default;
-    } */
-  @media print {
-    color-adjust: exact;
-  }
-`;
---
+++ src/components/Button/styles.ts
@@ -0,0 +1,102 @@
+import { css } from '@emotion/core';
+import { colors } from 'src/styles/themes';
+
+export const baseStyle = css`
+  display: inline-block;
+  align-items: center;
+  flex-shrink: 0;
+  justify-content: center;
+  position: relative;
+  background-color: ${colors.transparent};
+  border: 0;
+  margin: 0;
+  height: 28px;
+  line-height: 28px;
+  cursor: pointer;
+  user-select: none;
+  vertical-align: middle;
+  appearance: none;
+  text-decoration: none;
+  transition: background 120ms ease-in 0s;
+
+  white-space: nowrap;
+  border-width: 0;
+  border-radius: 3px;
+  box-shadow: none;
+  color: ${colors.grey800};
+  fill: ${colors.grey800};
+  padding: 0 8px;
+  font-size: 14px;
+  font-weight: 500;
+  outline: none;
+
+  &:hover {
+    background-color: ${colors.greyR50};
+  }
+  &:active {
+    background-color: ${colors.greyR100};
+  }
+  &::-moz-focus-inner {
+    border-style: none;
+  }
+  /* disabled {
+      pointerEvents: none;
+      cursor: default;
+    } */
+  @media print {
+    color-adjust: exact;
+  }
+`;
+export const type = {
+  Default: css`
+    background-color: ${colors.transparent};
+    box-shadow: none;
+    color: ${colors.grey800};
+    fill: ${colors.grey800};
+    &:hover {
+      background-color: ${colors.greyR50};
+    }
+    &:active {
+      background-color: ${colors.greyR100};
+    }
+  `,
+  Primary: css`
+    background-color: ${colors.primaryLight};
+    box-shadow: rgba(15, 15, 15, 0.1) 0px 0px 0px 1px inset, rgba(15, 15, 15, 0.1) 0px 1px 2px;
+    color: ${colors.white};
+    fill: ${colors.white};
+    &:hover {
+      background-color: ${colors.primary};
+    }
+    &:active {
+      background-color: ${colors.primaryDeep};
+    }
+  `,
+}
+
+export const size = {
+  Tiny: css`
+    height: 20px;
+    line-height: 20px;
+    font-size: 12px;
+    padding: 0 4px;
+  `,
+  Small: css`
+    height: 24px;
+    line-height: 24px;
+    font-size: 14px;
+    padding: 0 8px;
+  `,
+  Medium: css`
+    height: 28px;
+    line-height: 28px;
+    font-size: 14px;
+    padding: 0 8px;
+  `,
+  Big: css`
+    height: 32px;
+    line-height: 32px;
+    font-size: 14px;
+    padding: 0 8px;
+  `,
+}
