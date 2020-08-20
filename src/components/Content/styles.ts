314c512ed41d69b368887fcd74a26b692478f86d
Tamm <tmmoond8@gmail.com>
2020-08-20 11:48:33 +0900

    fix: Content Spacing margin error

--- src/components/Content/styles.ts
+++ src/components/Content/styles.ts
@@ -4,5 +4,7 @@ import { css, SerializedStyles } from '@emotion/core';
 export const spacing = (size: number): SerializedStyles => css`
   display: inline-block;
   width: 100%;
-  margin: ${size}px;
+  height: 0;
+  padding: 0;
+  margin: ${size}px 0 0 0;
 `;
